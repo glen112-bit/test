0ee7f134ed8b8dffd343424add1fbe498e093279
Glen112 <escobar.glen@gmail.com>
2024-04-09 22:23:38 -0300

    shop

--- src/components/carrito/index.js
+++ src/components/carrito/index.js
@@ -1,10 +1,17 @@
-  import React, {useState} from 'react';
+import { Item } from '../../components'
+import React, {useState} from 'react';
 
-  export const Carrito = () => {
-    
+  export const Carrito = ({producto, item}) => {
+     // console.log('producto:', producto);
       return (
       <>
         <div>Carrito</div>
+         <Item 
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            img = {item.img}
+         />
       </>
     );
 }
